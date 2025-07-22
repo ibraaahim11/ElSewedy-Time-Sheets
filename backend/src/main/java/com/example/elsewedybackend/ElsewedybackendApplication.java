f0d38005518a0eb60285c6c91fef6286738cde25
ibraaahim11 <ibrahimhesham21@gmail.com>
2025-07-22 14:01:35 +0300

    Initial Commit

---
+++ backend/src/main/java/com/example/elsewedybackend/ElsewedybackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.elsewedybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsewedybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsewedybackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/elsewedybackend/ElsewedybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elsewedybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsewedybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

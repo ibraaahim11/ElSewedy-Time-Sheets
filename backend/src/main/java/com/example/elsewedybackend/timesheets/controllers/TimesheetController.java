17d7fe8ac721d41eaefa068f6805288df6a3c341
ibraaahim11 <ibrahimhesham21@gmail.com>
2025-07-26 15:12:19 +0300

    Delete timsheet by id route

--- backend/src/main/java/com/example/elsewedybackend/timesheets/controllers/TimesheetController.java
+++ backend/src/main/java/com/example/elsewedybackend/timesheets/controllers/TimesheetController.java
@@ -3,6 +3,8 @@ package com.example.elsewedybackend.timesheets.controllers;
 
 import com.example.elsewedybackend.timesheets.entities.TimeSheet;
 import com.example.elsewedybackend.timesheets.repositories.TimesheetRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -18,14 +20,23 @@ public class TimesheetController {
 	}
 
 
+	@GetMapping("/{tsId}")
+	public Optional<TimeSheet> findFullTimeSheet(@PathVariable Integer tsId) {
+		return timesheetRepository.findById(tsId);
+	}
 
-	 @GetMapping("/{tsId}")
-		public Optional<TimeSheet> findFullTimeSheet(@PathVariable Integer tsId)
-	 {
-return timesheetRepository.findById(tsId);
-	 }
+	// fully deletes timesheet with accompanying data in other tables.
+	@DeleteMapping("/{tsId}")
+	public ResponseEntity<String> deleteTimeSheet(@PathVariable Integer tsId) {
+		Optional<TimeSheet> ts = timesheetRepository.findById(tsId);
 
-
+		if (ts.isPresent()) {
+			timesheetRepository.deleteById(tsId);
+			return ResponseEntity.ok("Timesheet deleted successfully.");
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Timesheet not found.");
+		}
+	}
 
 
 }
